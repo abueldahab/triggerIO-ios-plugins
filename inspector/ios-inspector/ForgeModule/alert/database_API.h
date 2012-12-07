e5d1866e67b4ab00e48e8fa5d99b42a15e87f693
Alex Horak <alexamh9@gmail.com>
2012-12-07 12:36:40 -0500

    Added fmdb

---
+++ inspector/ios-inspector/ForgeModule/alert/database_API.h
@@ -0,0 +1,13 @@
+//
+//  database_API.h
+//  ForgeModule
+//
+//  Created by explhorak on 12/7/12.
+//  Copyright (c) 2012 Trigger Corp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface database_API : NSObject
+
+@end
---
+++ inspector/ios-inspector/ForgeModule/alert/database_API.m
@@ -0,0 +1,13 @@
+//
+//  database_API.m
+//  ForgeModule
+//
+//  Created by explhorak on 12/7/12.
+//  Copyright (c) 2012 Trigger Corp. All rights reserved.
+//
+
+#import "database_API.h"
+
+@implementation database_API
+
+@end
