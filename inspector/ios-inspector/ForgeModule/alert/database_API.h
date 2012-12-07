5da61140a47ba6cc39792d9847fca8ac91431b90
Alex Horak <alexamh9@gmail.com>
2012-12-07 17:47:31 -0500

    Renamed all alerts to database to use in leu of making a new module

--- inspector/ios-inspector/ForgeModule/alert/database_API.h
+++ inspector/ios-inspector/ForgeModule/alert/database_API.h
@@ -1,13 +1,16 @@
 //
-//  database_API.h
-//  ForgeModule
+//  alert1_API.h
+//  ForgeInspector
 //
-//  Created by explhorak on 12/7/12.
+//  Created by Connor Dunn on 27/07/2012.
 //  Copyright (c) 2012 Trigger Corp. All rights reserved.
 //
+
 
 #import <Foundation/Foundation.h>
 
 @interface database_API : NSObject
+
++ (void)show:(ForgeTask*)task text:(NSString *)text;
 
 @end
--- inspector/ios-inspector/ForgeModule/alert/database_API.m
+++ inspector/ios-inspector/ForgeModule/alert/database_API.m
@@ -1,13 +1,27 @@
 //
-//  database_API.m
-//  ForgeModule
+//  alert_API.m
+//  ForgeInspector
 //
-//  Created by explhorak on 12/7/12.
+//  Created by Connor Dunn on 27/07/2012.
 //  Copyright (c) 2012 Trigger Corp. All rights reserved.
 //
 
 #import "database_API.h"
 
 @implementation database_API
+
++ (void)show:(ForgeTask*)task text:(NSString *)text {
+	if ([text length] == 0) {
+		[task error:@"You must enter a message"];
+		return;
+	}
+	UIAlertView *database = [[UIAlertView alloc] initWithTitle:@"Alert1"
+													message:text
+												   delegate:nil
+										  cancelButtonTitle:@"OK"
+										  otherButtonTitles:nil];
+	[database show];
+	[task success:nil];
+}
 
 @end
