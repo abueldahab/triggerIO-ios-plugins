260303cde9ee57d9f57f13933834077bdaa23c2d
Alex Horak <alexamh9@gmail.com>
2013-01-30 16:30:23 -0500

    Readme is now slightly more verbose

--- inspector/ios-inspector/ForgeModule/alert/kill_form_assist_API.m
+++ inspector/ios-inspector/ForgeModule/alert/kill_form_assist_API.m
@@ -12,6 +12,8 @@
 
 + (void)killBar:(ForgeTask *)task text:(NSString *)text {
     
+    NSLog(@"removing");
+    
     [[NSNotificationCenter defaultCenter] addObserverForName:@"UIKeyboardCandidateCorrectionDidChangeNotification" object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
         UIWindow *keyboardWindow = nil;
         for (UIWindow *testWindow in [[UIApplication sharedApplication] windows]) {
@@ -20,13 +22,26 @@
             }
         }
         
-        // Locate UIWebFormView.
+        // Locate UIWebFormViedw.
         for (UIView *possibleFormView in [keyboardWindow subviews]) {
             // iOS 5 sticks the UIWebFormView inside a UIPeripheralHostView.
             if ([[possibleFormView description] rangeOfString:@"UIPeripheralHostView"].location != NSNotFound) {
                 for (UIView *subviewWhichIsPossibleFormView in [possibleFormView subviews]) {
                     if ([[subviewWhichIsPossibleFormView description] rangeOfString:@"UIWebFormAccessory"].location != NSNotFound) {
+//                        [subviewWhichIsPossibleFormView removeFromSuperview];
+                        UIScrollView *webScroll;
+                        if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 5.0) {
+                            webScroll = [ForgeApp sharedApp].webView.scrollView;
+                        } else {
+                            webScroll = [[ForgeApp sharedApp].webView.subviews lastObject]; // iOS 2.x (?) - 4.x
+                        }
+                        CGRect newFrame = webScroll.frame;
+                        
+                        float accesssoryHeight = subviewWhichIsPossibleFormView.frame.size.height;
+                        newFrame.size.height += accesssoryHeight;
+                        
                         [subviewWhichIsPossibleFormView removeFromSuperview];
+                        [webScroll setFrame:newFrame];
                     }
                 }
             }
