da7960329480d235092c2d81e004806d0f7b59f0
Alex Horak <alexamh9@gmail.com>
2013-02-11 20:10:08 -0500

    Fixed the thin white line issue for iOS 6.0

--- inspector/ios-inspector/ForgeModule/alert/kill_form_assist_API.m
+++ inspector/ios-inspector/ForgeModule/alert/kill_form_assist_API.m
@@ -7,48 +7,48 @@
 //
 
 #import "kill_form_assist_API.h"
+#import <QuartzCore/QuartzCore.h>
 
 @implementation kill_form_assist_API
 
 + (void)killBar:(ForgeTask *)task text:(NSString *)text {
     
-    // Doesn't rip out bar on iOS 6 because doing so leaves a thin white line.
-    if ([[[UIDevice currentDevice] systemVersion] floatValue] != 6.0) {
-    
-        [[NSNotificationCenter defaultCenter] addObserverForName:@"UIKeyboardCandidateCorrectionDidChangeNotification" object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
-            UIWindow *keyboardWindow = nil;
-            for (UIWindow *testWindow in [[UIApplication sharedApplication] windows]) {
-                if (![[testWindow class] isEqual:[UIWindow class]]) {
-                    keyboardWindow = testWindow;
-                }
+    [[NSNotificationCenter defaultCenter] addObserverForName:@"UIKeyboardCandidateCorrectionDidChangeNotification" object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
+        UIWindow *keyboardWindow = nil;
+        for (UIWindow *testWindow in [[UIApplication sharedApplication] windows]) {
+            if (![[testWindow class] isEqual:[UIWindow class]]) {
+                keyboardWindow = testWindow;
             }
-            
-            // Locate UIWebFormViedw.
-            for (UIView *possibleFormView in [keyboardWindow subviews]) {
-                // iOS 5 sticks the UIWebFormView inside a UIPeripheralHostView.
-                if ([[possibleFormView description] rangeOfString:@"UIPeripheralHostView"].location != NSNotFound) {
-                    for (UIView *subviewWhichIsPossibleFormView in [possibleFormView subviews]) {
-                        if ([[subviewWhichIsPossibleFormView description] rangeOfString:@"UIWebFormAccessory"].location != NSNotFound) {
-                            // [subviewWhichIsPossibleFormView removeFromSuperview];
-                            UIScrollView *webScroll;
-                            if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 5.0) {
-                                webScroll = [ForgeApp sharedApp].webView.scrollView;
-                            } else {
-                                webScroll = [[ForgeApp sharedApp].webView.subviews lastObject]; // iOS 2.x (?) - 4.x
-                            }
-                            CGRect newFrame = webScroll.frame;
-                            
-                            float accesssoryHeight = subviewWhichIsPossibleFormView.frame.size.height;
-                            newFrame.size.height += accesssoryHeight;
-                            
-                            [subviewWhichIsPossibleFormView removeFromSuperview];
-                            [webScroll setFrame:newFrame];
+        }
+        
+        // Locate UIWebFormViedw.
+        for (UIView *possibleFormView in [keyboardWindow subviews]) {
+            // iOS 5 sticks the UIWebFormView inside a UIPeripheralHostView.
+            if ([[possibleFormView description] rangeOfString:@"UIPeripheralHostView"].location != NSNotFound) {
+                for (UIView *subviewWhichIsPossibleFormView in [possibleFormView subviews]) {
+                    if ([[subviewWhichIsPossibleFormView description] rangeOfString:@"UIWebFormAccessory"].location != NSNotFound) {
+                        // [subviewWhichIsPossibleFormView removeFromSuperview];
+                        UIScrollView *webScroll;
+                        if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 5.0) {
+                            webScroll = [ForgeApp sharedApp].webView.scrollView;
+                        } else {
+                            webScroll = [[ForgeApp sharedApp].webView.subviews lastObject]; // iOS 2.x (?) - 4.x
                         }
+                        CGRect newFrame = webScroll.frame;
+                        
+                        float accesssoryHeight = subviewWhichIsPossibleFormView.frame.size.height;
+                        newFrame.size.height += accesssoryHeight;
+                        
+                        [subviewWhichIsPossibleFormView removeFromSuperview];
+                        [webScroll setFrame:newFrame];
+                    }
+                    if ([[subviewWhichIsPossibleFormView description] rangeOfString:@"UIImageView"].location != NSNotFound) {
+                        [[subviewWhichIsPossibleFormView layer] setOpacity: 0.0];
                     }
                 }
             }
-        }];
-    }
+        }
+    }];
 }
 
 @end
